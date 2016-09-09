31d61b341c9222b4954d2de1936c71887cb1a1d5
Ankit <cosmic.ankit@gmail.com>
2016-09-09 21:50:01 +0530

    tables created

--- AnaLytIP/app/models.py
+++ AnaLytIP/app/models.py
@@ -3,14 +3,30 @@ Definition of models.
 """
 
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class StartUpInfo(models.Model):
-    Name=models.CharField
-    TeamMemebersNumber=models.CharField
-    About=models.CharField
-    HeadQuarters=models.CharField
-    ProductVideo=models.CharField
-    WebsiteLink=models.CharField
+    Name=models.CharField(max_length=100)
+    TeamMemebersNumber=models.CharField(max_length=100)
+    About=models.TextField()
+    HeadQuarters=models.CharField(max_length=100)
+    ProductVideo=models.CharField(max_length=100)
+    WebsiteLink=models.CharField(max_length=100)
+    Founders=models.CharField(max_length=100)
+    FundingInfo=models.TextField()
+    Team=models.CharField(max_length=50)
+    DateTime=models.DateTimeField()
+    
+class Activites(models.Model):
+    StartUpId=models.ForeignKey(StartUpInfo,on_delete=models.CASCADE)
+    Info=models.TextField()
+    TimeStamp=models.DateTimeField()
+
+class TechCategories(models.Model):
+    StartUpId=models.ForeignKey(StartUpInfo,on_delete=models.CASCADE)
+    Info=models.CharField(max_length=100)
+    TimeStamp=models.DateTimeField()
+
 
 
